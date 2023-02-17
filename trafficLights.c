bd7e44d8471a59d76c7a9f58b768a4e2ae8c78f0
Demo User <debian@beaglebone.localdomain>
2023-02-17 03:54:54 +0000

    Checkpointing

--- trafficLights.c
+++ trafficLights.c
@@ -10,17 +10,31 @@ void writeLED(char filename[], char port[], char value[]);
 
 int main() {
 	writeLED("/direction", "68", "out"); // set direction for pin
+	char trafficLight1Ports[] = ["67", "68", "44"];
+	char trafficLight2Ports[] = ["26", "46", "65"];
 	printf("Beginning to blink LED 5 times.\n");
-	for (int i =0; i < 5; i++) {
+	for (int i =0; i < 2; i++) {
 		writeLED("/value", "68", "1");
 		sleep(1);
 		writeLED("/value", "68", "0");
 		sleep(1);
 		printf("Blink %d\n", i);
-	}
+	} 
 	printf("Blinking complete.\n");	
 	
 	return 0;
+}
+
+void cycleLights() {
+	writeLED("/value", greenPort, "1");
+	sleep(120);
+	writeLED("/value", greenPort, "0");
+       	writeLED("/value", yellowPort, "1");
+	sleep(5);
+	writeLED("/value", yellowPort, "0");
+	writeLED("/value", redPort, "1");
+	sleep(125);
+	writeLED("/value", redPort, "0");	
 }
 
 void writeLED(char filename[], char port[], char value[]) {
