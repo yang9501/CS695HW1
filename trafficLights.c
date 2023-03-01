c6f0f39ea82ab59abfab7d3689f1331a6d8afc83
Justin Yang <justinyang@Justins-MBP.eduroam.gmu.edu>
2023-02-28 22:06:23 -0500

    MISRA refactoring

--- trafficLights.c
+++ trafficLights.c
@@ -7,7 +7,9 @@
 #include <sys/utsname.h>
 #include <stdint.h>
 
+//comment out to live run
 #define DEBUG 1
+
 #define GPIO_PATH_44 "/sys/class/gpio/gpio44" //Green 1
 #define GPIO_PATH_68 "/sys/class/gpio/gpio68" //Yellow 1
 #define GPIO_PATH_67 "/sys/class/gpio/gpio67" //Red 1
@@ -16,10 +18,13 @@
 #define GPIO_PATH_46 "/sys/class/gpio/gpio46" //Yellow 2
 #define GPIO_PATH_65 "/sys/class/gpio/gpio65" //Red 2
 
-void writeLED(uint8_t filename[], uint8_t port[], uint8_t value[]);
-void cycleLights(uint8_t greenPort1[], uint8_t yellowPort1[], uint8_t redPort1[], uint8_t greenPort2[], uint8_t yellowPort2[], uint8_t redPort2[]);
+//Writes specified value to specified GPIO directory
+static void writeLED(uint8_t *filename, uint8_t *port, uint8_t *value);
+//Primary light switch logic
+static void cycleLights(uint8_t *greenPort1, uint8_t *yellowPort1, uint8_t *redPort1, uint8_t *greenPort2, uint8_t *yellowPort2, uint8_t *redPort2);
 
 int main(void) {
+    //arrays containing GPIO port definitions, representing each of the two traffic lights
 	uint8_t trafficLight1Ports[3][25] = {GPIO_PATH_44, GPIO_PATH_68, GPIO_PATH_67};
 	uint8_t trafficLight2Ports[3][25] = {GPIO_PATH_26, GPIO_PATH_46, GPIO_PATH_65};
 
@@ -44,7 +49,7 @@ int main(void) {
 	return 0;
 }
 
-void cycleLights(uint8_t greenPort1[], uint8_t yellowPort1[], uint8_t redPort1[], uint8_t greenPort2[], uint8_t yellowPort2[], uint8_t redPort2[]) {
+static void cycleLights(uint8_t *greenPort1, uint8_t *yellowPort1, uint8_t *redPort1, uint8_t *greenPort2, uint8_t *yellowPort2, uint8_t *redPort2) {
     #ifdef DEBUG
     (void) printf("Green1 on: %s\n", greenPort1);
     (void) printf("Red2 on: %s\n", redPort2);
@@ -98,7 +103,7 @@ void cycleLights(uint8_t greenPort1[], uint8_t yellowPort1[], uint8_t redPort1[]
     #endif
 }
 
-void writeLED(uint8_t filename[], uint8_t port[], uint8_t value[]) {
+static void writeLED(uint8_t *filename, uint8_t *port, uint8_t *value) {
 	FILE* fp; //create file pointer
 	uint8_t fullFileName[100]; //store path and filename
     (void) sprintf(fullFileName, "%s%s", port, filename); //write path/name
